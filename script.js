9b4ea9140edcc04d7029a68da0e4e639b79b0f03
sameer <sameerkumar092005@gmail.com>
2025-11-20 23:44:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var inc = document.querySelector('#inc')
+var dec = document.querySelector('#dec')
+var h2 = document.querySelector('h2') 
+var count = 0;
+
+inc.addEventListener('click', function() {
+    count++;
+    h2.innerHTML = count;
+})  ; 
+dec.addEventListener('click', function() {
+    count--;
+    h2.innerHTML = count;
+});
